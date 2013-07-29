f9b8a33c38f3dbf44adc1bb88a9754652d1fe147
JT Olds <jt@spacemonkey.com>
2013-07-29 06:39:15 -0600

    space monkey internal commit export
    [katamari commit: 3f0543e5a824bc7f5b481971b59382abfd33d0e6]

--- errors.go
+++ errors.go
@@ -4,29 +4,22 @@ import (
     "fmt"
 )
 
-var root *ErrorClass
-
-func New(ec *ErrorClass) *ErrorClass {
-    return &ErrorClass{parent: ec}
-}
-
-func Wrap(err error) *Error {
-    return root.Wrap(err)
-}
-
 type ErrorClass struct {
     parent *ErrorClass
+    name   string
 }
 
-func (e *ErrorClass) New(format string, args ...interface{}) *Error {
-    return e.Wrap(fmt.Errorf(format, args...))
-}
+var (
+    // base error classes. To construct your own error class, use New.
+    SystemError       = &ErrorClass{parent: nil, name: "System Error"}
+    HierarchicalError = &ErrorClass{parent: nil, name: "Error"}
+)
 
-func (e *ErrorClass) Wrap(err error) *Error {
-    if err == nil {
-        return nil
+func New(ec *ErrorClass, name string) *ErrorClass {
+    if ec == nil {
+        ec = HierarchicalError
     }
-    return &Error{err: err, class: e}
+    return &ErrorClass{parent: ec, name: name}
 }
 
 func (e *ErrorClass) Is(parent *ErrorClass) bool {
@@ -43,18 +36,46 @@ type Error struct {
     class *ErrorClass
 }
 
-func (e *Error) Error() string {
-    return e.err.Error()
+func (e *ErrorClass) Wrap(err error) error {
+    if err == nil {
+        return nil
+    }
+    return &Error{err: err, class: e}
 }
 
-func (e *Error) Err() error {
+func (e *ErrorClass) New(format string, args ...interface{}) error {
+    return e.Wrap(fmt.Errorf(format, args...))
+}
+
+func (e *Error) Error() string {
+    return fmt.Sprintf("%s: %s", e.class.name, e.err.Error())
+}
+
+func (e *Error) WrappedErr() error {
     return e.err
+}
+
+func WrappedErr(err error) error {
+    cast, ok := err.(*Error)
+    if !ok {
+        return nil
+    }
+    return cast.WrappedErr()
 }
 
 func (e *Error) Is(ec *ErrorClass) bool {
     return e.class.Is(ec)
 }
 
+func (e *ErrorClass) Contains(err error) bool {
+    cast, ok := err.(*Error)
+    if !ok {
+        return SystemError == e
+    }
+    return cast.Is(e)
+}
+
 var (
-    NotImplementedError = New(nil)
+    // useful error classes
+    NotImplementedError = New(nil, "Not Implemented Error")
 )
